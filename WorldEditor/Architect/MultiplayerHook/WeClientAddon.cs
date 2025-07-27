f84115ca2175b74d08533762b44fe580415afa5f
arunkapila <arunj.kapila@gmail.com>
2025-07-27 12:39:04 +0100

    Incremented version number properly

--- WorldEditor/Architect/MultiplayerHook/WeClientAddon.cs
+++ WorldEditor/Architect/MultiplayerHook/WeClientAddon.cs
@@ -304,6 +304,6 @@ public class WeClientAddon : ClientAddon
     }
 
     protected override string Name => "Architect";
-    protected override string Version => "1.10.10.0";
+    protected override string Version => "1.11.0.0";
     public override bool NeedsNetwork => true;
 }
